2111aca789bb376fec251919b6b4a31fad667f09
Lali Mazzotta <lau.mazzotta@gmail.com>
2021-10-31 13:49:49 -0300

    desafio clase 3

---
+++ public/scripts.js
@@ -0,0 +1,4 @@
+let data = [1, 2, 3];
+let result = data.find(elem => elem > 1);
+
+console.log(result);
---
+++ src/Components/NavBar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import logo from './logo_green.svg';
+
+const NavBar = () => {
+    return (
+        <>
+        <nav className="navbar navbar-light bg-light fixed-top">
+            <div className="w-100 d-flex flex-column flex-lg-row align-items-center justify-content-center justify-content-lg-between flex-wrap">
+                <a className="navbar-brand m-0 m-lg-3 order-1 order-lg-0" href="index.html"><img src={logo} width="150" /></a>
+                <h4 className="text-primary text-center order-2 order-lg-0">Tienda online de productos sustentables y reciclados</h4>
+                
+                <form className="d-none d-lg-flex">
+                    <input className="form-control me-2" type="search" placeholder="Buscar producto" aria-label="Search" />
+                    <button className="btn btn-outline-primary" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>
+                    </button>
+                </form>
+                <button className="navbar-toggler border-0 align-self-end align-self-lg-center" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div className="offcanvas-header">
+                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
+                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3 text-primary">
+                            <li className="nav-item"><a className="nav-link" href="index.html">Productos</a></li>
+                            <li className="nav-item"><a className="nav-link" href="index.html">Mi cuenta</a></li>
+                            <li className="nav-item"><a className="nav-link" href="index.html">Quienes somos - Filosofía</a></li>
+                            <li className="nav-item"><a className="nav-link" href="index.html">Blog</a></li>
+                            <li className="nav-item"><a className="nav-link" href="index.html">Customer service</a></li>
+                            <li className="nav-item">
+                                <form className="d-lg-none d-flex nav-link">
+                                    <input className="form-control me-2" type="search" placeholder="Buscar producto" aria-label="Search" />
+                                    <button className="btn btn-outline-primary" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        
+        
+        </>
+    );
+}
+
+export default NavBar;
