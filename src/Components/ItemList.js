be63b68cd0d5a4d1a97547ab24ff868f21851e7f
Lali Mazzotta <lau.mazzotta@gmail.com>
2021-11-15 18:31:30 -0300

    spinner

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -1,10 +1,21 @@
 import Item from "./Item";
+import { css } from "@emotion/react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = css`
+  display: block;
+  margin: 5rem auto;
+  border-color: #006B2B;
+`;
 
 const ItemList = ({items}) => {
+
+    let color = ("#ffffff");
+
     return (
         items.length > 0
         ? items.map(item => <Item key={item.id} title={item.name} price={item.price} image={item.image} description={item.description} />)
-        : <p>Cargando productos...</p>
+        : <ClipLoader color={color} css={override} size={150} />
     );
 }
 
