b483d5db90e6097eb09c912575b1554f4554d7fa
Lali Mazzotta <lau.mazzotta@gmail.com>
2021-11-22 14:37:58 -0300

    primera entrega proyecto final

--- src/Components/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer.js
@@ -13,7 +13,7 @@ const ItemDetailContainer = () => {
             customFetch(2000, products.find(item => item.id === parseInt(idItem)))
                 .then(result => setDatos(result))
                 .catch(err => console.log(err))
-    }, []);
+    }, [idItem]);
 
     return (
         <>
